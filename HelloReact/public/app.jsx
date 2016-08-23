e7408acc2f09a9e941d61182f98e1d119958c41c
Nick Hayden <nick@nickphayden.com>
2016-08-23 22:48:47 +1000

    Add gitignore. Finish ch1. Make dynamic/stateful components.

--- HelloReact/public/app.jsx
+++ HelloReact/public/app.jsx
@@ -1,3 +1,53 @@
+var GreeterMessage = React.createClass({
+  render: function() {
+    var name = this.props.name;
+    var message = this.props.message;
+    return(
+      <div>
+        <h1>Hi {name}!</h1>
+        <p>{message}</p>
+      </div>
+    )
+  }
+});
+
+var GreeterForm = React.createClass({
+  onFormSubmit: function(e) {
+    e.preventDefault();
+    
+    var updates = {};
+  
+    var name = this.refs.name.value;
+    if (name.length > 0) {
+      this.refs.name.value = '';
+      updates.name = name;
+    }
+
+    var message = this.refs.message.value;
+    if (message.length > 0) {
+      this.refs.message.value = '';
+      updates.message = message;
+    }
+
+    this.props.onUpdates(updates);
+  },
+  render: function() {
+    return(
+        <form onSubmit={this.onFormSubmit}>
+          <div>
+            <input type="text" ref="name" placeholder="Enter name"/>
+          </div>
+          <div>
+            <textarea ref="message" placeholder="Enter message"></textarea>
+          </div>
+          <div>
+            <button>Submit Updates</button>
+          </div>
+        </form>
+    )
+  }
+});
+
 var Greeter = React.createClass({
   getDefaultProps: function() {
     return {
@@ -8,44 +58,32 @@ var Greeter = React.createClass({
   
   getInitialState: function () {
     return {
-      name: this.props.name
-    }  
+      name: this.props.name,
+      message: this.props.message
+    }
   },
 
-  onButtonClick: function (e) {
-    e.preventDefault();
-    var nameRef = this.refs.name;
-    var name = nameRef.value;
-    nameRef.value = '';
-
-    if (typeof name === 'string' && name.length > 0) {
-      this.setState({
-        name: name
-      });
-    }
+  handleUpdates: function (updates) {
+    this.setState(updates);
   },
 
   render: function() {
     var name = this.state.name;
-    var message = this.props.message;
+    var message = this.state.message;
+
     return (
       <div>
-        <h1>Hello {name}</h1>
-        <p>{message}</p>
-
-        <form onSubmit={this.onButtonClick}>
-          <input type="text" ref="name" />
-          <button>Set name print screen</button>
-        </form>
+        <GreeterMessage name={name} message={message} />
+        <GreeterForm onUpdates={this.handleUpdates} />
       </div>
     );
   }
 });
 
 var firstName = "Nick";
-var message1 = "This is my message";
+var message = "This is my message";
 
 ReactDOM.render(
-  <Greeter name={firstName} message={message1}/>,
+  <Greeter name={firstName} message={message} />,
   document.getElementById('app')
 );
