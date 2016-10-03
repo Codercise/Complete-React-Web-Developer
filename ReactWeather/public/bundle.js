21be3c8150e09c1dd1eeac2f416d226511b66c46
Nick Hayden <nick@nickphayden.com>
2016-10-03 20:25:16 +0900

    S5 L30 - Link and IndexLink

--- ReactWeather/public/bundle.js
+++ ReactWeather/public/bundle.js
@@ -26544,6 +26544,7 @@
 	var _require = __webpack_require__(172);
 
 	var Link = _require.Link;
+	var IndexLink = _require.IndexLink;
 
 
 	var Nav = React.createClass({
@@ -26559,18 +26560,20 @@
 	        'Nav Component'
 	      ),
 	      React.createElement(
-	        Link,
-	        { to: '/' },
+	        IndexLink,
+	        { to: '/', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
 	        'Get Weather'
 	      ),
+	      '-',
 	      React.createElement(
 	        Link,
-	        { to: '/about' },
+	        { to: '/about', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
 	        'About'
 	      ),
+	      '-',
 	      React.createElement(
 	        Link,
-	        { to: '/examples' },
+	        { to: '/examples', activeClassName: 'active', activeStyle: { fontWeight: 'bold' } },
 	        'Examples'
 	      )
 	    );
