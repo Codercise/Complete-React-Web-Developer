306dcd980ddf25e540f77cb182caa711e633567e
Nick Hayden <nick@nickphayden.com>
2016-10-03 20:15:34 +0900

    Finished video S5 L28 Create components, add navigation and basic functionality.

--- ReactBoilerplate/app/app.jsx
+++ ReactBoilerplate/app/app.jsx
@@ -1,6 +1,17 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var objOne = {
+  name: 'Nick',
+  location: 'Sendai'
+};
+
+var objTwo = {
+  age: 24,
+  ...objOne
+};
+
+console.log(objTwo);
 ReactDOM.render(
   <h1>Boilerplate App</h1>,
   document.getElementById('app')
