da77bae276c8157b40f8abb6c69121c439560781
Nick Hayden <nick@nickphayden.com>
2016-10-28 12:27:59 +1100

    Create ReactBoiletPlate2. Begin React Timer section.

---
+++ Timer/app/components/Navigation.jsx
@@ -0,0 +1,27 @@
+const React = require('react');
+const {Link, IndexLink} = require('react-router');
+
+const Navigation = () => {
+  return (
+    <div className="top-bar dark">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">React Timer App</li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/" activeClassName="active-link">Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">Created by <a href="#">Nick Hayden</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+module.exports = Navigation;
